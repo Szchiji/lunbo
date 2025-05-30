5c4118f7229f7bd234ed17bdd98bc4fc8c37b80d
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-05-30 19:39:35 +0800

    更新 bot_init.py

--- bot_init.py
+++ bot_init.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher()
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
