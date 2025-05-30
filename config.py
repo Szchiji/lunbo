b217f6bafd9d02101fa6350910806d09d60a19b9
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-05-30 19:59:53 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your-telegram-bot-token")
-WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
-WEBHOOK_URL = f"https://your-render-app.onrender.com{WEBHOOK_PATH}"
