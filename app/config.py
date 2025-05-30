72805af171e3ff4b56131af3f063eb3d78d0e34d
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-05-30 20:31:58 +0800

    更新 config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
-# app/config.py
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your-bot-token")
 DOMAIN = os.getenv("DOMAIN", "https://lunbo.onrender.com")
 WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook")
 WEBHOOK_URL = DOMAIN + WEBHOOK_PATH
+
+DATABASE_PATH = os.getenv("DATABASE_PATH", "sqlite:///./db.sqlite3")
