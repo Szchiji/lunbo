6b143004c20efe821b023395de1e1bc12bb1ebbc
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-05-30 19:40:48 +0800

    更新 utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def parse_datetime(text: str):
     try:
-        return datetime.strptime(text.strip(), "%Y-%m-%d %H:%M")
+        return datetime.strptime(text, "%Y-%m-%d %H:%M")
     except ValueError:
         return None
