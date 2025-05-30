4c91ac4324d3784dc918a3e83bdb8d57f016a6f1
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-05-30 19:39:03 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "你的 Bot Token")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://lunbo.onrender.com/webhook")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://yourdomain.com/webhook")
+DATABASE_PATH = "database.db"
