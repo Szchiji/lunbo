7d606eeb05e9e2f390e006d9e0b7db88e5c1bfa8
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-05-31 16:53:38 +0800

    Update states.py

--- app/states.py
+++ app/states.py
@@ -1,10 +1,10 @@
 from aiogram.fsm.state import StatesGroup, State
 
-class MessageStates(StatesGroup):
+class AddTaskState(StatesGroup):
     waiting_for_media = State()
-    waiting_for_text = State()
+    waiting_for_caption = State()
     waiting_for_buttons = State()
-    waiting_for_interval = State()
     waiting_for_start_time = State()
-    waiting_for_end_time = State()
-    confirmation = State()
+    waiting_for_stop_time = State()
+    waiting_for_interval = State()
+    confirm = State()
