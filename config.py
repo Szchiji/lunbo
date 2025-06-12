df17b63390bff7f33b4ec24c2e465adade644c3a
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-06-12 14:10:44 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-PORT = int(os.getenv("PORT", 10000))
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # e.g. https://yourapp.onrender.com
-IS_RENDER = os.getenv("RENDER", "").lower() == "true"
