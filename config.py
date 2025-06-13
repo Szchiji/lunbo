d73d0bf0a291a883aa40259f12b3e9cc6cb28d0e
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-06-13 20:46:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,12 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-GROUP_IDS = [int(i) for i in os.getenv("GROUP_IDS", "-1001234567890").split(",")]
+
+# 支持群名映射：GROUPS=-1001234567890:群1,-1009876543210:群2
+GROUPS = dict(
+    i.split(":") for i in os.getenv("GROUPS", "-1001234567890:测试群").split(",")
+)
+# 群ID强制转为int
+GROUPS = {int(k): v for k, v in GROUPS.items()}
+
 POSTGRES_DSN = os.getenv("POSTGRES_DSN")
